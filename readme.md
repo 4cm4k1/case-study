6a12670f79f019438b4f174e9aed78226d01835d
Anthony Maki <4cm4k1@gmail.com>
2018-07-27 13:56:26 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,40 @@
+# Website [![Status](https://img.shields.io/website-up-down-green-red/https/case-study.anthony.codes.svg?label=status&style=flat-square)](https://case-study.anthony.codes) [![Prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://prettier.io/) [![Renovate](https://img.shields.io/badge/renovate-enabled-1f8ceb.svg?style=flat-square)](https://renovatebot.com/) [![Latest Release](https://img.shields.io/github/release/4cm4k1/case-study/all.svg?style=flat-square)](https://github.com/4cm4k1/case-study/releases) [![License](https://img.shields.io/github/license/4cm4k1/case-study.svg?style=flat-square)](license)
+
+Case study for job application.
+
+## Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Code of Conduct](#code-of-conduct)
+- [Contributing](#contributing)
+- [Contributors](#contributors)
+- [Changelog](#changelog)
+- [License](#license)
+
+## Prerequisites
+
+TBD.
+
+## Installation
+
+TBD.
+
+## Usage
+
+TBD.
+
+## [Code of Conduct](.github/code_of_conduct.md)
+
+## [Contributing](.github/contributing.md)
+
+## Contributors
+
+| Name             | Website                 |
+| ---------------- | ----------------------- |
+| **Anthony Maki** | <https://anthony.codes> |
+
+## [Changelog](changelog.md)
+
+## [License](license)
---
+++ server.js
@@ -0,0 +1,85 @@
+const shrinkRay = require("shrink-ray-current");
+const polka = require("polka");
+const next = require("next");
+const { parse } = require("url");
+const { join } = require("path");
+const { createServer } = require("https");
+const { readFileSync } = require("fs");
+
+const { PORT = 4242, NODE_ENV } = process.env;
+
+const dev = NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const options = {
+  key: readFileSync("./key.pem"),
+  cert: readFileSync("./cert.pem")
+};
+
+const rootStaticFiles = [
+  "/android-chrome-192x192.png",
+  "/android-chrome-512x512.png",
+  "/apple-touch-icon.png",
+  "/browserconfig.xml",
+  "/favicon-16x16.png",
+  "/favicon-32x32.png",
+  "/favicon.ico",
+  "/manifest.json",
+  "/metadata.html",
+  "/mstile-150x150.png",
+  "/og-image.jpg",
+  "/robots.txt",
+  "/safari-pinned-tab.svg",
+  "/sitemap.xml"
+];
+
+async function setHeaders(req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://case-study.anthony.codes"
+  );
+  res.setHeader("Cache-Control", "max-age=31536000");
+  res.setHeader(
+    "Content-Security-Policy",
+    "style-src 'self'; " +
+      "script-src 'self' 'unsafe-inline'; " +
+      "font-src 'self'; " +
+      "base-uri 'self';"
+  );
+  res.setHeader(
+    "Strict-Transport-Security",
+    "max-age=63072000; includeSubDomains; preload"
+  );
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("X-UA-Compatible", "IE=edge; chrome=1");
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+  next();
+}
+
+// Set up Next
+app.prepare().then(() => {
+  // Set up Polka
+  const { handler } = polka()
+    // Use shrinkRay for awesome compression
+    .use(setHeaders, shrinkRay())
+    // Handle all requests
+    .get("*", async (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      const { path, query } = parsedUrl;
+
+      if (rootStaticFiles.includes(path)) {
+        // Handle requests for files in /static/
+        app.serveStatic(req, res, join(__dirname, "static", path));
+      } else {
+        // Handle everything else
+        handle(req, res);
+      }
+    });
+
+  // Start server
+  createServer(options, handler).listen(PORT, _ =>
+    console.log(`> Ready on https://localhost:${PORT}`)
+  );
+});
